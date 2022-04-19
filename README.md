4be7a46d3e27497390e4e59b3d2972e9b189c273
StBran0912 <sbran0912@gmail.com>
2022-04-19 14:17:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stbran0912geolocation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stbran0912geolocation)
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const nedb = require('nedb');
+const db = new nedb({ filename: 'locations.db', autoload: true });
+
+app.use(express.json({ limit: '1mb' }));
+
+//Statisches Verzeichnis setzen
+app.use(express.static('public'));
+
+app.use('/', (req, res, next) => {
+  console.log(
+    `Zugriff am: ${new Date().toLocaleString()}     von Client: ${req.ip}`
+  );
+  next();
+});
+
+app.post('/api', (req, res) => {
+  db.insert(req.body);
+  res.send({ status: 'Server meldet Post-Anfrage bearbeitet' });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server gestartet an Port ${PORT}`));
